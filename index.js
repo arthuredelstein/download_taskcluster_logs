9202884f82cd5311551539460ea965bc9f18556a
Arthur Edelstein <arthuredelstein@gmail.com>
2017-08-13 00:09:18 -0700

    log errors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let taskcluster = require('taskcluster-client');
 let request = require('request');
 let fs = require('fs');
 
-let TASK_GROUP_ID = 'GasvOlwbQa65-rbvjThzmA';
+let TASK_GROUP_ID = 'cz1GoajhTM29AKwxMsHqSw';
 
 async function main() {
   let queue = new taskcluster.Queue();
@@ -18,12 +18,19 @@ async function main() {
     group.tasks.forEach(async task => {
       let url = await queue.buildUrl(queue.getLatestArtifact, task.status.taskId, 'public/logs/live.log')
       console.log(url);
+      try {
       let log = request({url: url, gzip: true})
         .on('error', function(err) {
           console.log('unable to fetch logs for ' + task.status.taskId);
           console.log(err)
         })
         .pipe(fs.createWriteStream("results/" + task.status.taskId))
+          .on('error', function(err) {
+            console.log(err)
+          });
+      } catch (e) {
+        console.log(e.message);
+      }
     });
   } while (continuationToken)
 }
